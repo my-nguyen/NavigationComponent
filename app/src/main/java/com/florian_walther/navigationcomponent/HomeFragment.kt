0d2c2dca1e0f64e789dde517cc8dd7f3c001bf45
My Nguyen <nguyen_my@yahoo.com>
2021-03-27 17:53:38 -0700

    Part 3

--- app/src/main/java/com/florian_walther/navigationcomponent/HomeFragment.kt
+++ app/src/main/java/com/florian_walther/navigationcomponent/HomeFragment.kt
@@ -1,6 +1,19 @@
 package com.florian_walther.navigationcomponent
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.florian_walther.navigationcomponent.databinding.FragmentHomeBinding
 
 class HomeFragment: Fragment(R.layout.fragment_home) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentHomeBinding.bind(view)
+        binding.btnLogin.setOnClickListener {
+            val action = HomeFragmentDirections.actionHomeFragmentToLoginFragment()
+            findNavController().navigate(action)
+        }
+    }
 }
---
+++ app/src/main/java/com/florian_walther/navigationcomponent/LoginFragment.kt
@@ -0,0 +1,6 @@
+package com.florian_walther.navigationcomponent
+
+import androidx.fragment.app.Fragment
+
+class LoginFragment: Fragment(R.layout.fragment_login) {
+}
