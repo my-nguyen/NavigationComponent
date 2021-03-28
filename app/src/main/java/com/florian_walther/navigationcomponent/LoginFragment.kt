d578c2dc35be874acd4a6034dcd3ab73821ab94c
My Nguyen <nguyen_my@yahoo.com>
2021-03-28 16:15:17 -0700

    Part 9: deep links

--- app/src/main/java/com/florian_walther/navigationcomponent/LoginFragment.kt
+++ app/src/main/java/com/florian_walther/navigationcomponent/LoginFragment.kt
@@ -4,14 +4,20 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.florian_walther.navigationcomponent.databinding.FragmentLoginBinding
 
 class LoginFragment: Fragment(R.layout.fragment_login) {
+
+    private val args: LoginFragmentArgs by navArgs()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val binding = FragmentLoginBinding.bind(view)
+        val usernameDeepLink = args.username
         binding.apply {
+            edUsername.setText(usernameDeepLink)
             btConfirm.setOnClickListener {
                 val username = edUsername.text.toString()
                 val password = edPassword.text.toString()
