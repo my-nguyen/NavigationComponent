08bfe4de89b4b61cba6f53786bd5df58b90830e0
My Nguyen <nguyen_my@yahoo.com>
2021-03-27 21:24:35 -0700

    Part 5

--- app/src/main/java/com/florian_walther/navigationcomponent/MainActivity.kt
+++ app/src/main/java/com/florian_walther/navigationcomponent/MainActivity.kt
@@ -2,10 +2,33 @@ package com.florian_walther.navigationcomponent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.florian_walther.navigationcomponent.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // calling findNavController() will cause a crash. must code like below instead
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
+        setSupportActionBar(binding.toolbar)
+        // connect the toolbar to the navController
+        setupActionBarWithNavController(navController)
+    }
+
+    // for the Up button
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
